5222385ed121e0779e2f9e4424e9cb9f7c361876
Gatoprogramador888 <143905112+Gatoprogramador888@users.noreply.github.com>
2026-07-31 23:08:14 -0600

    pedir menos ram e imprimir si lavalink si se encendio

--- Infra/Lavalink/entrypoint.sh
+++ Infra/Lavalink/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
-# Forzamos el límite estricto de heap para Java a 350MB
-java -Xmx350m -Xms128m -XX:+UseG1GC -jar /app/Infra/Lavalink.jar &
+
+echo "=== Iniciando Lavalink ==="
+# Ejecutar Lavalink e imprimir su log
+java -Xmx300m -jar /app/Infra/Lavalink/Lavalink.jar > /app/lavalink.log 2>&1 &
+
+# Esperar 15 segundos
 sleep 15
-# Iniciar el bot de .NET
+
+# Si Lavalink se murió en esos 15s, mostrar el log de Java
+if ! kill -0 $! 2>/dev/null; then
+    echo "❌ CRASH DE LAVALINK DETECTADO. Mostrando logs de Java:"
+    cat /app/lavalink.log
+    exit 1
+fi
+
+echo "=== Lavalink está vivo. Iniciando Bot de .NET ==="
 exec dotnet DiscordBot.dll
