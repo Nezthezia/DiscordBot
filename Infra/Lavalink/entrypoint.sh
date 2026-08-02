e802df67b8fc24b4618d82814918e0de72a48a7a
Gatoprogramador888 <143905112+Gatoprogramador888@users.noreply.github.com>
2026-08-01 18:47:31 -0600

    obligar a lavalink a usar el puerto 2333 en vez de uno que le de render

--- Infra/Lavalink/entrypoint.sh
+++ Infra/Lavalink/entrypoint.sh
@@ -7,7 +7,7 @@ echo "=========================================="
 cd /app/Infra/Lavalink
 
 echo "🚀 Arrancando Lavalink en segundo plano..."
-java -Xms128m -Xmx200m -XX:+UseSerialGC -jar Lavalink.jar &
+java -Xms128m -Xmx200m -XX:+UseSerialGC -jar Lavalink.jar --server.port=2333 &
 
 echo "⏳ Esperando a que Lavalink esté listo en puerto 2333..."
 until (echo > /dev/tcp/localhost/2333) 2>/dev/null; do
