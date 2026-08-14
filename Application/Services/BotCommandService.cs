1700866ebdb9186f26b0ac56bd6948abe3228158
Gatoprogramador888 <143905112+Gatoprogramador888@users.noreply.github.com>
2026-08-14 15:01:53 -0600

    Eliminar el entrypoint.sh, el yml, los nombres en el codigo y dejarlo como plantilla

--- Application/Services/BotCommandService.cs
+++ Application/Services/BotCommandService.cs
@@ -22,32 +22,6 @@ public class BotCommandService : IBotCommandService
             return Task.FromResult<string?>($"¡Hola Mundo desde .NET 10 y Clean Architecture, {userMention}! 🚀");
         }
 
-        if (content.Equals($"{CommandInit}adios", StringComparison.OrdinalIgnoreCase))
-        {
-            _logger.LogInformation("Comando !hola recibido de {User}", username);
-            return Task.FromResult<string?>($"¡Nos vemos {userMention} espero verte pronto!");
-        }
-
-        if (content.Equals("paola", StringComparison.OrdinalIgnoreCase))
-        {
-            _logger.LogInformation("Enviar foto de la admin");
-            return Task.FromResult<string?>("foto no disponible actualmente");
-        }else if (content.Equals("goku", StringComparison.OrdinalIgnoreCase) ||
-            content.Equals("alex", StringComparison.OrdinalIgnoreCase))
-        {
-            _logger.LogInformation("Enviar foto del programador");
-            return Task.FromResult<string?>("https://klipy.com/gifs/mujikcboro-seriymujik-1");
-        } else if(content.Equals("joel", StringComparison.OrdinalIgnoreCase))
-        {
-            _logger.LogInformation("Enviar foto del joel");
-            return Task.FromResult<string?>("https://cdn.discordapp.com/attachments/1536721828679127131/1536918975491866664/Perfil_de_alias_Shadow-Anime.webp?ex=6a7d26d6&is=6a7bd556&hm=a59d7bc1178232d598ffad5e277531372e0b5c3f3b9e31606da8e712637c5a45");
-        }
-        else if(content.Equals("ema", StringComparison.OrdinalIgnoreCase))
-        {
-            _logger.LogInformation("Enviar foto del gato naranja llamado ema");
-            return Task.FromResult<string?>("https://klipy.com/gifs/thatscrazydude");
-        }
-
         return Task.FromResult<string?>(null);
     }
 }
